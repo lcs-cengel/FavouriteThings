5d5362147535aaa4b2615162e376731e26ce3e4f
Connor Engel <cengel@lcs.on.ca>
2024-12-12 12:32:50 -0500

    Neil helped, almost done

---
+++ FavouriteThings/DetailsView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailsView.swift
+//  FavouriteThings
+//
+//  Created by Connor Engel on 2024-12-12.
+//
+
+import SwiftUI
+
+struct DetailsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailsView()
+}
